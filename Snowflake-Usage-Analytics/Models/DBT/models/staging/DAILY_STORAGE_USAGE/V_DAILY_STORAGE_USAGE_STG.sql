4b7a605dca96722d626beae26ccbe937048dbeb1
Guilherme Alcantara <guilhermealc@hotmail.com>
2022-03-11 11:14:57 -0300

    Warehouse and storage metrics

---
+++ Snowflake-Usage-Analytics/Models/DBT/models/staging/DAILY_STORAGE_USAGE/V_DAILY_STORAGE_USAGE_STG.sql
@@ -0,0 +1,31 @@
+{{ config(
+    transient=false,
+    materialized= 'view',
+    schema= 'SNOWFLAKE_USAGE',
+    tags= ["staging", "daily"]
+    ) 
+}}
+ 
+
+WITH source AS (
+  SELECT * FROM  {{source('ACCOUNT_USAGE','STORAGE_USAGE')}}
+),
+
+rename as (
+   SELECT 
+    MD5(S.USAGE_DATE::STRING) AS K_STORAGE_USAGE_DLHK
+    ,S.USAGE_DATE
+    ,S.STORAGE_BYTES AS M_STORAGE_BYTES
+    ,S.STAGE_BYTES AS M_STAGE_BYTES
+    ,S.FAILSAFE_BYTES AS M_FAILSAFE_BYTES
+
+    ,STORAGE_BYTES / power(1024, 4)  AS M_STORAGE_TERABYTES
+    ,STAGE_BYTES / power(1024, 4)  AS M_STAGE_TERABYTES
+    ,FAILSAFE_BYTES / power(1024, 4)   AS M_FAILSAFE_TERABYTES
+
+    ,(STORAGE_BYTES + STAGE_BYTES + FAILSAFE_BYTES) / power(1024, 4) as M_BILLABLE_TB
+
+FROM source S
+)
+
+SELECT * FROM rename
