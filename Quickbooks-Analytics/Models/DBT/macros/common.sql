3498731900f186b077cee7a7e8655d73d5928f9b
Guilherme Alcantara <guilhermealc@hotmail.com>
2022-03-23 10:41:25 -0300

    update submodules

--- Quickbooks-Analytics/Models/DBT/macros/common.sql
+++
@@ -1,4 +0,0 @@
---creating full name by concatenating first and last name
-{% macro full_name(firstName,middleName, lastName) %}
-    (COALESCE({{firstName}},'') || ' ' || COALESCE({{middleName}} || ' ','') || COALESCE({{lastName}},''))
-{% endmacro %}
